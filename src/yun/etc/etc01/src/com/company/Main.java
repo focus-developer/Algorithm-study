cbd98e9d902c32b0660773db924b2426a1b2f3a1
LeeYunJae <yunjea0312@naver.com>
2019-07-11 17:18:15 +0900

    Add: Etc01

---
+++ src/yun/etc/etc01/src/com/company/Main.java
@@ -0,0 +1,63 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int arr[] = {1,2,3,3,3,4,4};
+
+        int answer [] = solution(arr);
+
+        for(int i=0; i<answer.length; i++) {
+            System.out.println(answer[i]);
+        }
+
+    }
+
+    public static int [] solution(int[] param) {
+        int [] answer = {};
+
+        List<Integer> list = new ArrayList<Integer>();
+
+        int k = 0;
+
+        boolean check = false;
+
+        for(int i=0; i<param.length; i++) {
+            int target = param[i];
+            int count = 1;
+            for(int j=i+1; j<param.length; j++) {
+                if(target != 0) {
+                    if(target == param[j]) {
+                        count++;
+                        param[j] = 0;
+
+                    }
+                }
+                if(j == param.length-1) {
+                    if(count > 1) {
+                        list.add(count);
+                        check = true;
+                    }
+                }
+            }
+        }
+
+        if(check) {
+            answer = new int[list.size()];
+            for(int i=0; i<list.size(); i++) {
+                answer[i] = list.get(i);
+            }
+        }else {
+            answer = new int[1];
+            answer[0] = -1;
+        }
+
+
+        return answer;
+    }
+}
