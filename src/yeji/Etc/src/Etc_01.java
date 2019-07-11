dd2e0a45d206a65a87d219ea2074ad0b46926623
LeeYeJi <lwj1748@naver.com>
2019-07-11 20:23:59 +0900

    Fix: etc01

--- src/yeji/Etc/src/Etc_01.java
+++ src/yeji/Etc/src/Etc_01.java
@@ -1,64 +1,53 @@
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.TreeMap;
+import java.util.*;
 
 public class Etc_01 {
 	public static void run() {
-		//int[] v = {1,2,3,3,3,4,4};
-        //int[] v = {3,2,4,4,2,5,2,5,5};
-        //int[] v = {3,5,7,9,1};
-        int[] v = {5,5,5,5,1,10,10,20,1};
+		// int[] v = {1,2,3,3,3,4,4};
+		//int[] v = { 3, 2, 4, 4, 2, 5, 2, 5, 5 };
+		// int[] v = {3,5,7,9,1};
+		int[] v = {5,5,5,5,1,10,10,10,20,1};
 
-        int[] answer = solution(v);
+		int[] answer = solution(v);
 
-        for(int i=0;i<answer.length;i++)
-            System.out.print(answer[i]+" ");
-        System.out.println("");
+		for (int i = 0; i < answer.length; i++)
+			System.out.print(answer[i] + " ");
+		System.out.println("");
 	}
-	
-	public static int[] solution(int[] param){
-        int[] answer = {};
-        HashMap<Integer,Integer> count = new HashMap<>();
 
-        for(int i=0;i<param.length;i++){
-            if(count.get(param[i]) == null){
-                count.put(param[i],1);
-            }
-            else{
-                int tmp = count.get(param[i])+1;
-                count.put(param[i],tmp);
-            }
-        }
+	public static int[] solution(int[] param) {
+		int[] answer = {};
 
-        TreeMap<Integer,Integer> sortedMap = new TreeMap<>(count);
-        Iterator<Integer> sortedMapIterator = sortedMap.keySet().iterator();
-        int overlapCount = 0;
-        HashMap<Integer,Integer> overlapSave = new HashMap<>();
-        while(sortedMapIterator.hasNext()){
-            Integer key = sortedMapIterator.next();
-            if(sortedMap.get(key)>1) {
-                overlapCount++;
-                overlapSave.put(overlapCount,sortedMap.get(key));
-            }
-        }
+		LinkedHashMap<Integer, Integer> overlapList = new LinkedHashMap<>();
 
-        if(overlapCount==0) {
-            answer = new int[1];
-            answer[0] = -1;
-            return answer;
-        }
-        else {
-            answer = new int[overlapCount];
-            Iterator<Integer> overlapSaveIterator = overlapSave.keySet().iterator();
+		ArrayList<Integer> overlapCount = new ArrayList<>();
 
-            while(overlapSaveIterator.hasNext()){
-                Integer key = overlapSaveIterator.next();
-                answer[key-1] = overlapSave.get(key);
-            }
+		for (int i = 0; i < param.length; i++) {
+			if (overlapList.get(param[i]) != null)
+				overlapList.put(param[i], overlapList.get(param[i]) + 1);
+			else
+				overlapList.put(param[i], 1);
+		}
 
-            return answer;
-        }
+		Iterator<Integer> iterator = overlapList.keySet().iterator();
+		while (iterator.hasNext()) {
+			int key = iterator.next();
+			if (overlapList.get(key) > 1)
+				overlapCount.add(overlapList.get(key));
+		}
+		
+		if(overlapCount.size()==0) {
+			answer = new int[1];
+			answer[0] = -1;
+		}
+		else {
+			answer = new int[overlapCount.size()];
+			for(int i=0;i<overlapCount.size();i++) {
+				answer[i] = overlapCount.get(i);
+			}
+		}
 
-    }
+		return answer;
+
+	}
 
 }
